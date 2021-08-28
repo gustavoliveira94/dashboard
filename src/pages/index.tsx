4bf29ab27c4d70dadf62d946c57fd2b570e76307
Gustavo Oliveira <gmacedo@mcontigo.com>
2021-08-28 06:02:45 -0300

    feat: resume component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,8 @@
 import type { NextPage } from 'next';
-import { useQuery } from '@apollo/client';
-
-import { GET_WEALTH_ID } from 'services/graphqlClient/queries/wealth';
 
 import HomeScreen from 'screens/Home';
 
 const Home: NextPage = () => {
-  const { data } = useQuery(GET_WEALTH_ID, { variables: { id: 1 } });
-
-  console.log(data);
-
   return <HomeScreen />;
 };
 
